a868eefc04b6de9add55802153c3abc51fe9b3eb
Max <max@maxf.net>
2024-02-29 17:32:30 +0000

    updated

--- test/authn.js
+++ test/authn.js
@@ -16,12 +16,35 @@ describe('authn', async function() {
 			access_token: process.env.EZSSO_ADMIN_ACCESS_TOKEN
 		};
 		expect(this.context?.access_token).to.exist;
+		this.ezssoAdmin = new EZSSOAdmin(this.context);
 	});
 
 	it('ls', async function () {
-		const ezssoAdmin = new EZSSOAdmin(this.context);
-		const authns = await ezssoAdmin.authn.ls();
-		debug('authns:', authns);
+		const authns = await this.ezssoAdmin.authn.ls();
+	});
+
+	it('new', async function () {
+		const newAuthN = await this.ezssoAdmin.authn.new();
+		const allAuthNs = await this.ezssoAdmin.authn.ls();
+		const filtered = allAuthNs.filter(authn => authn.id == newAuthN.id);
+		expect(filtered[0]?.id).to.equal(newAuthN.id);
+	});
+
+	it('del', async function () {
+		const newAuthN = await this.ezssoAdmin.authn.new();
+		let allAuthNs = await this.ezssoAdmin.authn.ls();
+		let filtered = allAuthNs.filter(authn => authn.id == newAuthN.id);
+		expect(filtered[0]?.id).to.equal(newAuthN.id);
+
+		const delAuthN = await this.ezssoAdmin.authn.del(newAuthN.id);
+		debug('delAuthN:', delAuthN);
+
+		allAuthNs = await this.ezssoAdmin.authn.ls();
+		debug('allAuthNs:', allAuthNs);
+
+		filtered = allAuthNs.filter(authn => authn.id == newAuthN.id);
+		debug('filtered:', filtered);
+		expect(filtered.length).to.equal(0);
 	});
 
 });
